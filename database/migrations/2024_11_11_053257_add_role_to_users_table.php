1a18f0acc987faadf7f750d41953c46bcd48a85c
cristianCAOM <21610060@utgz.edu.mx>
2024-11-21 18:03:19 -0600

    commit 33

--- database/migrations/2024_11_11_053257_add_role_to_users_table.php
+++ database/migrations/2024_11_11_053257_add_role_to_users_table.php
@@ -14,7 +14,9 @@ class AddRoleToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('role')->default('user')->after('email');
+            if (!Schema::hasColumn('users', 'role')) {
+                $table->string('role')->default('user')->after('email');
+            }
         });
     }
 
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,8 +6,9 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
-                    </a>
+                        <img src="../images/logoempresa.png" alt="" class="h-12 w-auto">
+{{--                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+ --}}                    </a>
                 </div>
 
                 <!-- Navigation Links -->
